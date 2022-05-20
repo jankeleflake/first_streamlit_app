f411360ab3491befee42d7d5586a35c60778624c
jankeleflake <105917046+jankeleflake@users.noreply.github.com>
2022-05-20 13:23:57 +0200

    Update streamlit_app.py
    added breakfast nemu header and added 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('My parents new healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bleuberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
